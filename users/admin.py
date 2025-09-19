402943820ddb3e12094da0041a3000873761f9fe
Oleksii Kostenikov <akotenikov@gmail.com>
2025-09-19 23:14:17 +0300

    Added first_name and last_name to admin add_fieldsets

--- users/admin.py
+++ users/admin.py
@@ -34,6 +34,7 @@ class UserAdmin(DjangoUserAdmin):
                 "fields": ("email", "password1", "password2"),
             },
         ),
+        (_("Personal info"), {"fields": ("first_name", "last_name")}),
     )
     list_display = ("email", "first_name", "last_name", "is_staff")
     search_fields = ("email", "first_name", "last_name")
