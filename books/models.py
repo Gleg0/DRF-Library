34ce978281813b5372c15d706f9a9510b5085d0c
Hamster <dtaranenko137@gmail.com>
2025-09-20 00:41:51 +0300

    fix model (add cover field), linter

--- books/models.py
+++ books/models.py
@@ -2,8 +2,13 @@ from django.db import models
 
 
 class Book(models.Model):
+    class Cover(models.TextChoices):
+        HARD = "HARD", "Hard"
+        SOFT = "SOFT", "Soft"
+
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
+    cover = models.CharField(max_length=5, choices=Cover.choices)
     inventory = models.PositiveIntegerField()
     daily_fee = models.DecimalField(max_digits=7, decimal_places=2)
 
