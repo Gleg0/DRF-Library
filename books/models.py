6af6a78db37a38108783ddc8e62ce4dbd0ff7339
Hamster <dtaranenko137@gmail.com>
2025-09-20 01:02:17 +0300

    make min value

--- books/models.py
+++ books/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
 
@@ -10,7 +11,14 @@ class Book(models.Model):
     author = models.CharField(max_length=255)
     cover = models.CharField(max_length=5, choices=Cover.choices)
     inventory = models.PositiveIntegerField()
-    daily_fee = models.DecimalField(max_digits=7, decimal_places=2)
+    daily_fee = models.DecimalField(
+        max_digits=7,
+        decimal_places=2,
+        validators=(MinValueValidator(0.01),),
+    )
 
     def __str__(self):
-        return f"{self.title} - price per day {self.daily_fee}$. {self.inventory} is available"
+        return (
+            f"{self.title} - price per day {self.daily_fee}$."
+            f" {self.inventory} is available"
+        )
