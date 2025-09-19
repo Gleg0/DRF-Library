a7afcd38043d0d344c507721a0c29a13fab10360
Hamster <dtaranenko137@gmail.com>
2025-09-19 21:27:59 +0300

    create model Book
    # Conflicts:
    #	requirements.txt

--- books/models.py
+++ books/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    inventory = models.PositiveIntegerField()
+    daily_fee = models.DecimalField(max_digits=7, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.title} - price per day {self.daily_fee}$. {self.inventory} is available"
