75c32227776ce754a5ea67998ab2cabfff83aa75
Glego <glego200323@gmail.com>
2025-09-19 21:08:09 +0300

    Change development database.

---
+++ config/settings/prod.py
@@ -0,0 +1,14 @@
+from decouple import config
+
+from config.settings.base import *  # noqa: F403,F405
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("DB_NAME"),
+        "USER": config("DB_USER"),
+        "PASSWORD": config("DB_PASSWORD"),
+        "HOST": config("DB_HOST", default="localhost"),
+        "PORT": config("DB_PORT", default="5432"),
+    }
+}
