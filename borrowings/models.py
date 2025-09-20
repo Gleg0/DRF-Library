614c7fd14ce6e1e58482501bda9219ca5638fc45
Hamster <dtaranenko137@gmail.com>
2025-09-20 13:10:29 +0300

    add str method

--- borrowings/models.py
+++ borrowings/models.py
@@ -18,3 +18,6 @@ class Borrowing(models.Model):
         on_delete=models.CASCADE,
         related_name="borrowings",
     )
+
+    def __str__(self):
+        return f"{self.user} take {self.book}"
