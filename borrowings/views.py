63971f2bb606972db3bd7f67f42037d503caca6c
MakovM <makovinskiy09@gmail.com>
2025-09-20 18:13:17 +0300

    Add create borrowing endpoint

--- borrowings/views.py
+++ borrowings/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets
 
 from borrowings.models import Borrowing
 from borrowings.serializers import (
+    BorrowingCreateSerializer,
     BorrowingDetailSerializer,
     BorrowingListSerializer,
     BorrowingSerializer,
@@ -11,9 +12,21 @@ from borrowings.serializers import (
 class BorrowingViewSet(viewsets.ModelViewSet):
     queryset = Borrowing.objects.all()
 
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def get_queryset(self):
+        queryset = self.queryset
+
+        if self.action == "list":
+            queryset = queryset.filter(user=self.request.user)
+        return queryset
+
     def get_serializer_class(self):
         if self.action == "list":
             return BorrowingListSerializer
         elif self.action == "retrieve":
             return BorrowingDetailSerializer
+        elif self.action == "create":
+            return BorrowingCreateSerializer
         return BorrowingSerializer
