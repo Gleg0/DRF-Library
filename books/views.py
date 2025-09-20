b90193fe93282366cdad2e7ec5f91724619ea49b
Hamster <dtaranenko137@gmail.com>
2025-09-20 18:04:57 +0300

    fix permissions

--- books/views.py
+++ books/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
 
 from base.permissions import IsAdminOrIfAuthenticatedReadOnly
 from books.models import Book
@@ -7,9 +8,13 @@ from books.serializers import BookListSerializer, BookSerializer
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
-    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
     def get_serializer_class(self):
         if self.action == "list":
             return BookListSerializer
         return BookSerializer
+
+    def get_permissions(self):
+        if self.action == "list":
+            return (AllowAny,)
+        return (IsAdminOrIfAuthenticatedReadOnly,)
