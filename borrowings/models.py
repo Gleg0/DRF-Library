d214470896bfe23c4a5c7ce107dcc4c3e460c019
Hamster <dtaranenko137@gmail.com>
2025-09-20 11:39:47 +0300

    add to admin panel

--- borrowings/models.py
+++ borrowings/models.py
@@ -7,13 +7,14 @@ from config.settings import base
 class Borrowing(models.Model):
     borrow_date = models.DateField(auto_now_add=True)
     expected_return = models.DateField()
-    actual_return_date = models.DateField()
+    actual_return_date = models.DateField(blank=True, null=True)
     book = models.ForeignKey(
         Book,
         on_delete=models.CASCADE,
-        related_name="borrowing"
+        related_name="borrowings",
     )
     user = models.ForeignKey(
         base.AUTH_USER_MODEL,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        related_name="borrowings",
     )
