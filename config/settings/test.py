35c5726f5c89170baab2c32ac562449b98fffcc1
Glego <glego200323@gmail.com>
2025-09-22 21:59:34 +0300

    Set up test settings.

---
+++ config/settings/test.py
@@ -0,0 +1,36 @@
+from datetime import timedelta
+
+from config.settings.base import *  # noqa: F403,F405
+
+DEBUG = config("DEBUG", default=True, cast=bool)
+SECRET_KEY = config("SECRET_KEY", default="dev-secret")
+ALLOWED_HOSTS = config(
+    "ALLOWED_HOSTS",
+    default="127.0.0.1,localhost",
+    cast=lambda v: [s.strip() for s in v.split(",")],
+)
+
+INSTALLED_APPS += ["django_extensions"]  # noqa: F403,F405
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
+    }
+}
+
+del(REST_FRAMEWORK["DEFAULT_THROTTLE_CLASSES"])
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=7),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    "ROTATE_REFRESH_TOKENS": True,
+    "BLACKLIST_AFTER_ROTATION": False,
+}
+
+MESSAGE_TELEGRAM_BOT_TOKEN = config(
+    "TELEGRAM_BOT_TOKEN", default="telegram-token"
+)
+
+STRIPE_SECRET_KEY = config("STRIPE_SECRET_KEY")
+STRIPE_PUBLISHABLE_KEY = config("STRIPE_PUBLISHABLE_KEY")
--- manage.py
+++ manage.py
@@ -8,9 +8,11 @@ from decouple import config
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault(
-        "DJANGO_SETTINGS_MODULE", config("DJANGO_SETTINGS_MODULE")
-    )
+    if "test" in sys.argv:
+        os.environ["DJANGO_SETTINGS_MODULE"] = "config.settings.test"
+    else:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.dev")
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
