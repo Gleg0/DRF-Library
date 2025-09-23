668a0128cbbc458410a4171efac566006906aa59
Oleksii Kostenikov <akotenikov@gmail.com>
2025-09-23 11:15:08 +0300

    Removed unnecessary comments

--- config/urls.py
+++ config/urls.py
@@ -6,7 +6,6 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 docs_urlpatterns = [
     path("schema/", SpectacularAPIView.as_view(), name="schema"),
-    # Optional UI:
     path(
         "swagger/",
         SpectacularSwaggerView.as_view(url_name="schema"),
