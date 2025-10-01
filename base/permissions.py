c9d760b5976721f90784cb509e1ae81b7c958d53
Hamster <dtaranenko137@gmail.com>
2025-10-01 11:44:54 +0300

    change docs

--- base/permissions.py
+++ base/permissions.py
@@ -3,8 +3,6 @@ from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 class IsAdminOrIfAuthenticatedReadOnly(BasePermission):
     def has_permission(self, request, view):
-        if (
+        return (
             request.method in SAFE_METHODS and request.user.is_authenticated
-        ) or request.user.is_staff:
-            return True
-        return False
+        ) or request.user.is_staff
