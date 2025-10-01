239aa5aeeebf7f078f76eba0643e3f55a33ba1c6
Glego <glego200323@gmail.com>
2025-10-01 12:08:52 +0300

    Clean telegram bot code.

--- notifications/management/commands/run_bot.py
+++ notifications/management/commands/run_bot.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 
-from notifications.bot_runner import run
+from notifications.services.bot.bot_runner import run
 
 
 class Command(BaseCommand):
